f5d746aebf1dbd08842327d4ddd2a7483cc21866
RKL13 <raphael@koudache-lou.vet>
2022-07-04 17:31:45 +0200

    requirements update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 sudo apt update
 
-sudo apt install python3 python3-pip
+sudo apt install python3 python3-pip unzip
 sudo apt-get install python3-venv
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -13,6 +13,7 @@ source ./venv/bin/activate
 
 pip3 install -r requirements.txt
 
+pipenv install jupyterlab
 jupyter notebook --generate-config
 jupyter notebook password
 jupyter notebook --ip 0.0.0.0 --no-browser
